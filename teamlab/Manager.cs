7389fbd75a3021bfe11332bfeaeb1975f2595162
DanielTkachenko <pdg.danila.98@mail.ru>
2022-05-01 23:03:55 +0700

    Init commit

---
+++ teamlab/Manager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Reflection;
+
+namespace teamlab
+{
+    class Manager
+    {
+        Schedule scheduler = new Schedule();
+
+        public void Parse(string path)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(path);
+            XmlElement? xRoot = xmlDoc.DocumentElement;
+            if (xRoot != null)
+            {
+                foreach (XmlElement task in xRoot)
+                {
+                    XmlNodeList? task_childs = task.ChildNodes;
+                    /*Assembly asm = Assembly.LoadFrom(task.Attributes.GetNamedItem("assemblyname").Value);
+                    Type? taskType = asm.GetType(task.Attributes.GetNamedItem("classname").Value);*/
+
+                    if (task_childs != null)
+                    {
+                        string typeName = Assembly.GetExecutingAssembly().GetName().Name + "." + 
+                            task.Attributes.GetNamedItem("classname").Value;
+                        
+                        MyTask tmp = (MyTask)Assembly.GetExecutingAssembly().CreateInstance(typeName);
+                        foreach (XmlElement attrs in task_childs)
+                        {
+                            if (attrs.Name == "schedule")
+                            {
+                                foreach (XmlNode sh in attrs.ChildNodes)
+                                {
+                                    string schedule_str = sh.InnerText;
+                                    if (schedule_str == "OnStart")
+                                    {
+                                        scheduler.AddOnStart(tmp);
+                                    }
+                                    else if (schedule_str.Contains("OnTime"))
+                                    {
+                                        int hours = Int32.Parse(schedule_str.Substring(schedule_str.IndexOf(";") + 1, 2));
+                                        int mins = Int32.Parse(schedule_str.Substring(schedule_str.IndexOf(":") + 1, 2));
+                                        DateTime time = new DateTime();
+                                        time = time.AddHours(hours);
+                                        time = time.AddMinutes(mins);
+                                        scheduler.AddOnTime(tmp, time);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void RunSchedule()
+        {
+            scheduler.Run();
+        }
+    }
+}
---
+++ teamlab/Schedule.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace teamlab
+{
+    class Schedule
+    {
+        List<MyTask> onStartList;
+        Dictionary<MyTask, DateTime> onTimeList;
+
+        public Schedule()
+        {
+            onStartList = new List<MyTask>();
+            onTimeList = new Dictionary<MyTask, DateTime>();
+        }
+
+        public void AddOnStart(MyTask task)
+        {
+            onStartList.Add(task);
+        }
+
+        public void AddOnTime(MyTask task, DateTime time)
+        {
+            onTimeList.Add(task, time);
+        }
+
+        public void Run()
+        {
+            foreach(MyTask t in onStartList)   //запуск сервисов с параметром OnStart
+            {
+                t.Run();
+            }
+            
+            while(true)                     //запуск цикла с проверкой времени для запуска задач в определенное время
+            {
+                DateTime now = DateTime.Now;
+                foreach(var t in onTimeList)
+                {
+                    if (now.Hour == t.Value.Hour && now.Minute == t.Value.Minute)
+                    {
+                        t.Key.Run();
+                        onTimeList.Remove(t.Key);
+                    }
+                }
+                Thread.Sleep(30000);        //проверяем расписание каждые 30 сек
+            }
+        }
+    }
+}
