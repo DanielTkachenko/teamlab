82ca8bf42174cf0d24c69a50c42db9b19e4df944
DanielTkachenko <pdg.danila.98@mail.ru>
2022-05-01 23:29:48 +0700

    Init commit

---
+++ teamlab/MyTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teamlab
+{
+    class MyTask : IRunable
+    {
+        public virtual void Run()
+        {
+            Console.WriteLine("MyTask is running!");
+            Console.WriteLine(DateTime.Today);
+            Console.WriteLine("MyTask is stopped!");
+        }
+    }
+
+    class MyTaskOne : MyTask
+    {
+        public override void Run()
+        {
+            Console.WriteLine("MyTaskOne is running!");
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("MyTaskOne is running!");
+        }
+    }
+
+    class MyTaskTwo : MyTask
+    {
+        public override void Run()
+        {
+            Console.WriteLine("MyTaskTwo is running!");
+            Console.WriteLine(DateTime.UtcNow);
+            Console.WriteLine("MyTaskTwo is running!");
+        }
+    }
+}
